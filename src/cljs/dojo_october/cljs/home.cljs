39922f5f36a4a09e52f70935850304f999ad3f74
James Henderson <james@pcr.me.uk>
2013-10-29 20:30:44 +0000

    Rendering random numbers and colours

--- src/cljs/dojo_october/cljs/home.cljs
+++ src/cljs/dojo_october/cljs/home.cljs
@@ -2,21 +2,24 @@
   (:require [dommy.core :as d])
   (:require-macros [dommy.macros :refer [node sel1]]))
 
-(defn render-graph [canvas]
-  (let [data [{:value 30
-               :color "#F38630"}
-              {:value 25
-               :color "#008630"}
-              {:value 11
-               :color "#AA8630"}]
-        ctx (.getContext canvas "2d")]
+(defn render-graph [canvas data]
+  (let [ctx (.getContext canvas "2d")]
     (.Pie (js/Chart. ctx) (clj->js data) (clj->js {}))))
 
+(defn rand-color []
+  (str "rgb(" (rand-int 255) "," (rand-int 255) "," (rand-int 255) ")"))
 
+(defn random-value []
+  {:value (rand-int 100)
+   :color (rand-color)})
+
+(defn sample-data []
+  (repeatedly 3 random-value))
 
 (defn watch-hash! [!hash]
   (add-watch !hash :home-page
              (fn [_ _ _ hash]
                (when (= "#/" hash)
-                 (d/replace-contents! (sel1 :#content) (doto (node [:canvas {:height 400 :width 400}])
-                                                         (render-graph)))))))
+                 (d/replace-contents! (sel1 :#content) (doto (node [:canvas {:height 400 :width 400 :style {:margin-top "2em"}}])
+                                                         (render-graph (sample-data))))))))
+
